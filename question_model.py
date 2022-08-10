4593938e4cfd53910400da053340c88cc4e57f9d
Arlind <arlindshaqiri@hotmail.com>
2022-08-10 02:31:50 +0200

    more files added and question model changed

--- question_model.py
+++ question_model.py
@@ -1,14 +1,10 @@
 class Question:
     def __init__(self, text, answer):
-        print("\nNew function")
         self.text = text
         self.answer = answer
 
 
-new_question = Question("texti1", "pergjigjia1")
+new_question = Question("Pyetja", "Pergjigjia")
 print(new_question.text)
 print(new_question.answer)
 
-new_question2 = Question("texti2", "pergjigjia2")
-print(new_question2.text)
-print(new_question2.answer)
